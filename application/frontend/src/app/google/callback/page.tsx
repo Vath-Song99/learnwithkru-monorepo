628bf18540ce86d689c1b7ad1cda385555970da1
Smoeury Songvat <vatgaming287@gmail.com>
2024-05-21 15:53:38 +0700

    teacher cards and signup with google

---
+++ application/frontend/src/app/google/callback/page.tsx
@@ -0,0 +1,49 @@
+// pages/callback.js
+"use client"
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+const CallbackRedirect = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  useEffect(() => {
+
+    const code = searchParams.get('code');
+
+    if (code) {
+      // Code is present, send it to your backend to exchange for access token
+      exchangeCodeForToken(code as string);
+    } else {
+      // Code is not present, handle error or redirect accordingly
+      console.error('No authorization code found');
+      // Redirect to error page or home page
+      router.push('/google/callback');
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router]);
+
+  const exchangeCodeForToken = async (code: string) => {
+    try {
+      const response = await fetch(`http://localhost:3000/v1/auth/google/callback?code=${code}`);
+      const data = await response.json();
+      // Handle successful token exchange, maybe store token in localStorage or cookies
+      console.log('Token data:', data);
+      // Redirect to dashboard or profile page
+      router.push('/teacher-list');
+    } catch (error) {
+      console.error('Error exchanging code for token:', error);
+      // Redirect to error page or home page
+      router.push('/');
+    }
+  };
+
+  return (
+    <div>
+      <p className=''
+      >Redirecting...</p>
+    </div>
+  );
+};
+
+export default CallbackRedirect;
