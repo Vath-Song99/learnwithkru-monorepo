6a987373081f9162686971ded631d7ce8fae6d42
sea porhai <seaporhai@gmail.com>
2024-05-28 14:23:30 +0700

    student unauthurize

--- application/frontend/src/context/CardContext.tsx
+++ application/frontend/src/context/CardContext.tsx
@@ -44,7 +44,7 @@ const CardContext = ({ children }: { children: any }) => {
         const { data } = await handleRequestTeacher();
         // Check if teachers is an array
         if (Array.isArray(data)) {
-          setData(data  ); // Update state with fetched data
+          setData(data); // Update state with fetched data
         } else {
           console.error("Expected an array of data but got:", data);
         } // Update state with fetched data
@@ -57,18 +57,18 @@ const CardContext = ({ children }: { children: any }) => {
   }, []);
 
 
-  const handleRequestTeacher = async () => {
-    try {
+    const handleRequestTeacher = async () => {
+      try {
 
-      const API_ENDPOINT = "http://localhost:3000/v1/teachers"; // Replace with your actual token
-      const response = await axios.get(API_ENDPOINT, { withCredentials: true });
+        const API_ENDPOINT = "http://localhost:3000/v1/teachers"; // Replace with your actual token
+        const response = await axios.get(API_ENDPOINT, { withCredentials: true });
 
-      return response.data;
-    } catch (error: any) {
-      console.error("Error fetching teachers:", error);
-      throw error;
-    }
-  };
+        return response.data;
+      } catch (error: any) {
+        console.error("Error fetching teachers:", error);
+        throw error;
+      }
+    };
   // const toggleFavorite = (id: string) => {
   //   setData((prevData) => {
   //     if (!id) return prevData; // Check if item is undefined
@@ -82,10 +82,9 @@ const CardContext = ({ children }: { children: any }) => {
   //     return newData;
   //   });
   // };
- 
-//  =========================================================================================
-//  student Fetching
- const [studentData , setStudentData] = useState([])
+
+  //  =========================================================================================
+
 
   const contextvalue = {
     Data,
