ae0ac900730d09793b44ea6a3a0a961af527c22f
sea porhai <seaporhai@gmail.com>
2024-05-29 14:50:45 +0700

    fix almost done

--- application/frontend/src/components/organisms/navbar/TeacherNavbar.tsx
+++ application/frontend/src/components/organisms/navbar/TeacherNavbar.tsx
@@ -8,10 +8,10 @@ const TeacherNavbar = ({
   className?: string;
 }) => {
   return (
-    <div
-      className={` flex justify-center sm:justify-center md:justify-center items-start w-full h-[60px] mt-7 bg-[#F4F4F8]`}>
-      <div className=" flex justify-evenly items-start p-3">{children}</div>
+    <div className={`flex justify-start md:justify-center lg:justify-between items-start h-[60px] mt-7 w-full md:w-[80%] lg:w-[90%] xl:w-[90%] mx-auto ${className}`}>
+      <div className="flex justify-center items-start p-3">{children}</div>
     </div>
+
   );
 };
 
