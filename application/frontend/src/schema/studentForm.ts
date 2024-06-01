5ca584d826f3d3306095e8b066b7899a41fc2a35
sea porhai <seaporhai@gmail.com>
2024-06-01 16:21:22 +0700

    done fetch data student with post method

--- application/frontend/src/schema/studentForm.ts
+++ application/frontend/src/schema/studentForm.ts
@@ -1,24 +1,13 @@
 import * as yup from "yup";
 
 const studentSchema = yup.object().shape({
-  school_name: yup
-    .string()
+  school_name: yup.string()
     .required("School name is required")
-    .min(3, "School name must be at least 3 characters long"),
-  // student_card: yup
-  //   .mixed()
-  //   .test("fileSize", "File size is too large", (value: any) => {
-  //     return value && value.size <= 200000; // 9MB
-  //   }),
-  grade: yup
-    .number()
-    .required("Grade is required")
-    .min(1, "Grade must be at least 1")
-    .max(12, "Grade must be at most 12"),
-  education: yup
-    .string()
-    .required("Education is required")
-    .max(60, "Education must be at most 60 characters long"),
+    .min(3, "School name must be at least 3 characters long")
+    .max(50, "School name cannot exceed 50 characters"),
+  education: yup.string().required("Education level is required"),
+  grade: yup.string().required("Grade is required"),
+  student_card: yup.mixed().notRequired(),
 });
 
 export { studentSchema };
