0c82cfc013344beef1142a1284bd6d0af68bceea
smoeury-songvate <vatgaming287@gmail.com>
2024-06-04 23:00:06 +0700

    feat: cookie

--- application/frontend/src/app/page.tsx
+++ application/frontend/src/app/page.tsx
@@ -1,36 +1,45 @@
 import { Homepage, Navbar } from "@/components";
 import axios from "axios";
+import { cookies } from "next/headers";
 
 const getUserData = async () => {
+  "use server";
   try {
+    const cookiesStore = cookies();
+    const cookiesData = cookiesStore.getAll();
+    console.log("Cookies: ", cookiesData);
+
     const res = await axios.get("http://localhost:3000/v1/users/user-profile", {
-      withCredentials: true,
+      headers: {
+        Cookie: `${cookiesData[0].name}=${cookiesData[0].value};${cookiesData[1].name}=${cookiesData[1].value};${cookiesData[2].name}=${cookiesData[2].value}`,
+      },
     });
-    if (res.data.data) {
-      throw new Error(res.data.data);
-    }
+    // if (res.data.data) {
+    //   throw new Error(res.data.data);
+    // }
     return res.data;
   } catch (error: unknown) {
     throw error;
   }
 };
-const Page = () => {
-  const data = getUserData();
+const Page = async () => {
+  const data = await getUserData();
 
   if (!data) {
     return (
       <div className="w-full flex justify-center pt-10">
         <div className="flex justify-center items-center min-h-screen">
-          <div className="animate-spin rounded-full h-9 w-9 border-t-4 border-[#7B2CBF]"></div>
+          <div className="animate-spin rounded-full h-9 w-9 borderinvoked-t-4 border-[#7B2CBF]"></div>
         </div>
       </div>
     ); // Render loading state
   }
-
   const authState = {
-    isAuth: true,
+    isAuth: !!data,
     user: data,
   };
+
+  console.log("Thi is auth state: ", authState);
   return (
     <div className="max-w-full grid">
       <div className="w-full flex justify-center items-center">
