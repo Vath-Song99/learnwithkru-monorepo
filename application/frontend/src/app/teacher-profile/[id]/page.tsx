9550e65bc4e81fabb1850e9eab8ea3b71a5bfe04
smoeury-songvate <vatgaming287@gmail.com>
2024-06-04 21:17:06 +0700

    feat: teacher

--- application/frontend/src/app/teacher-profile/[id]/page.tsx
+++ application/frontend/src/app/teacher-profile/[id]/page.tsx
@@ -1,89 +1,89 @@
-/* eslint-disable react/jsx-no-duplicate-props */
-"use client";
+// /* eslint-disable react/jsx-no-duplicate-props */
+// "use client";
 
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
 
-const Page = ({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}) => {
-  const [isShowModal, setIsShowModal] = useState<boolean>(false);
-  const [authState, setAuthState] = useState<{ isAuth: boolean; user: any }>({
-    isAuth: false,
-    user: null,
-  });
-  const [isLoading, setIsLoading] = useState(true);
-  const { id } = params;
+// const Page = ({
+//   params,
+// }: {
+//   params: {
+//     id: string;
+//   };
+// }) => {
+//   const [isShowModal, setIsShowModal] = useState<boolean>(false);
+//   const [authState, setAuthState] = useState<{ isAuth: boolean; user: any }>({
+//     isAuth: false,
+//     user: null,
+//   });
+//   const [isLoading, setIsLoading] = useState(true);
+//   const { id } = params;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const url = "http://localhost:3000/v1/users/user-profile";
-        const data = await handleUserProfile(url);
-        console.log("User profile data:", data);
-        if (!data.data) {
-          console.log("User not found!", data.data);
-        }
-        setAuthState({
-          isAuth: true,
-          user: data.data,
-        });
-      } catch (error) {
-        console.error("Error fetching user profile:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const url = "http://localhost:3000/v1/users/user-profile";
+//         const data = await handleUserProfile(url);
+//         console.log("User profile data:", data);
+//         if (!data.data) {
+//           console.log("User not found!", data.data);
+//         }
+//         setAuthState({
+//           isAuth: true,
+//           user: data.data,
+//         });
+//       } catch (error) {
+//         console.error("Error fetching user profile:", error);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     };
 
-    if (id) {
-      fetchData();
-    }
-  }, [id]);
+//     if (id) {
+//       fetchData();
+//     }
+//   }, [id]);
 
-  const handleUserProfile = async (url: string): Promise<any> => {
-    try {
-      const response = await axios.get(url, { withCredentials: true });
-      if (response.data.errors) {
-        console.log("An error occurred:", response.data.errors);
-      }
-      return response.data;
-    } catch (error: unknown) {
-      console.error("Error fetching data:", error);
-      throw error;
-    }
-  };
+//   const handleUserProfile = async (url: string): Promise<any> => {
+//     try {
+//       const response = await axios.get(url, { withCredentials: true });
+//       if (response.data.errors) {
+//         console.log("An error occurred:", response.data.errors);
+//       }
+//       return response.data;
+//     } catch (error: unknown) {
+//       console.error("Error fetching data:", error);
+//       throw error;
+//     }
+//   };
 
-  if (isLoading) {
-    return (
-      <div className="w-full flex justify-center pt-10">
-        <div className="flex justify-center items-center min-h-screen">
-          <div className="animate-spin rounded-full h-9 w-9 border-t-4 border-[#7B2CBF]"></div>
-        </div>
-      </div>
-    );
-  }
+//   if (isLoading) {
+//     return (
+//       <div className="w-full flex justify-center pt-10">
+//         <div className="flex justify-center items-center min-h-screen">
+//           <div className="animate-spin rounded-full h-9 w-9 border-t-4 border-[#7B2CBF]"></div>
+//         </div>
+//       </div>
+//     );
+//   }
 
-  return (
-    <div className="max-w-full h-[200vh]">
-      <div className="w-full flex justify-center items-center">
-        <Navbar
-          setIsShowModal={setIsShowModal}
-          isShowModal={isShowModal}
-          authState={authState}
-        />
-      </div>
-      <div className="w-full flex justify-center">
-        <TeachersProfile teacherId={id as string} />
-      </div>
-      <div className="w-full flex justify-center items-start bg-gray-900 mt-10">
-        <Footer />
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="max-w-full h-[200vh]">
+//       <div className="w-full flex justify-center items-center">
+//         <Navbar
+//           setIsShowModal={setIsShowModal}
+//           isShowModal={isShowModal}
+//           authState={authState}
+//         />
+//       </div>
+//       <div className="w-full flex justify-center">
+//         <TeachersProfile teacherId={id as string} />
+//       </div>
+//       <div className="w-full flex justify-center items-start bg-gray-900 mt-10">
+//         <Footer />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
