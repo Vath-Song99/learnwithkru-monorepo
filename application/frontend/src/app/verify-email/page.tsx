61818cb88fe508e86f3657db89ba0c3a6c8727ef
Smoeury Songvat <vatgaming287@gmail.com>
2024-06-05 08:56:14 +0700

    feat: SSR

--- application/frontend/src/app/verify-email/page.tsx
+++ application/frontend/src/app/verify-email/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 import { VerifyEmailToken } from "@/components";
-import { UserProvider } from "@/context/UserContext";
 import React from "react";
 
 const Page = () => {
  
   return (
-    <UserProvider>
     <div className="max-w-full grid">
       <div className="w-full flex justify-center items-center">
       </div>
       <VerifyEmailToken/>
       <div className="w-full flex justify-center items-start bg-gray-900"></div>
     </div>
-    </UserProvider>
   );
 };
 
