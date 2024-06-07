fb171c59808233315bd8d94b72821709fe1bc1e3
smoeury-songvate <vatgaming287@gmail.com>
2024-06-07 23:32:43 +0700

    fix: gateway

--- packages/api-gateway/src/app.ts
+++ packages/api-gateway/src/app.ts
@@ -76,7 +76,7 @@ app.use("*", (req: Request, res: Response, _next: NextFunction) => {
     .json({ message: "The endpoint called does not exist." });
 });
 
-// Erorr Handling
+// Erorr Handler
 app.use(errorHandler);
 
 export default app;
