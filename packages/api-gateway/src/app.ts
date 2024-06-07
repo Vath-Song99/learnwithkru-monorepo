75ba674f05e8c9b1833d8b119d5f98857024bea0
Smoeury Songvat <vatgaming287@gmail.com>
2024-06-07 15:17:51 +0700

    feat: Gateaway Deployment

--- packages/api-gateway/src/app.ts
+++ packages/api-gateway/src/app.ts
@@ -76,7 +76,7 @@ app.use("*", (req: Request, res: Response, _next: NextFunction) => {
     .json({ message: "The endpoint called does not exist." });
 });
 
-// Error handler
+// Error handler catch all error
 app.use(errorHandler);
 
 export default app;
