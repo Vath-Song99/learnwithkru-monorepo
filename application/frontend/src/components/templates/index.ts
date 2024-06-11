b7276eaf00d09c2035f04fe4743421022d28b3cb
thon1525 <thonhourn1525@gmail.com>
2024-06-11 09:55:19 +0700

    remove becomeTeachres

--- application/frontend/src/components/templates/index.ts
+++ application/frontend/src/components/templates/index.ts
@@ -5,7 +5,7 @@ import SettingProfile from "./SettingsProfile";
 import { CreateClass } from "./CreateClass";
 import ResetPassword from "./ResetPassword";
 import { BecomeTeacher } from "./Become-teacher";
-import { TeacherInfomation } from "./TeacherInfomation";
+
 
 export {
   Homepage,
@@ -15,5 +15,4 @@ export {
   CreateClass,
   ResetPassword,
   BecomeTeacher,
-  TeacherInfomation
 };
