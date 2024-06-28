a719797e0b2cfb6b9698316965f438aa42e5da18
Smoeury Songvat <vatgaming287@gmail.com>
2024-06-28 08:34:06 +0700

    feat: message

--- applications/frontend/src/app/layout.tsx
+++ applications/frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import NextTopLoader from "nextjs-toploader";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: "400",
@@ -21,17 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={poppins.className}>
-        <NextTopLoader
-          color="#2299DD"
-          initialPosition={0.08}
-          crawlSpeed={200}
-          height={3}
-          crawl={true}
-          showSpinner={true}
-          easing="ease"
-          speed={200}
-          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-        />
+       
         {children}
       </body>
     </html>
